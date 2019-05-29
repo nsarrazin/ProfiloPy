6def79bc470b425e88b9fb92638353bdddd72378
Nathan Sarrazin <sarrazin.nathan@gmail.com>
2019-05-29 14:27:09 +0200

    Updated the data manager with some documentation

--- DataManager.py
+++ DataManager.py
@@ -2,6 +2,11 @@ import numpy as np
 import numba as nb
 import json
 
+@nb.jit(nopython=True, cache=True)
+def find_nearest(array, value):
+    array = np.array(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
 
 def file_to_json(filepath, t0, cutoff_experiment=10, cutoff_datapoint=25, resample=1000, maxdt=10, file_out = "", debug=False):
     """This function converts a raw .txt obtained from the profilometer into an organized JSON that can be parsed easily. 
@@ -81,6 +86,48 @@ def file_to_json(filepath, t0, cutoff_experiment=10, cutoff_datapoint=25, resamp
     with open(file_out, 'w') as outfile:
         json.dump(listExperiments, outfile)
                 
+class DataManager:
+    def __init__(self, json_path):
+        """This class reads a json file generated from `file_to_json` and loads into memory so that it can be called later.
+        
+        Arguments:
+            json_path {[str]} -- [Path to the json file to be loaded in the data manager]
+        """
+        with open(json_path, "rb") as json_file:
+            self.data = json.load(json_file)
+
+    def get_step(self, time, experiment=0):
+        """This functions parses through the raw data and returns the data whose timestamp is the closest to the specified time
+        
+        Arguments:
+            time {[float]} -- [The time whose nearest slice you want to extract]
+        
+        Keyword Arguments:
+            experiment {int} -- [The experiment index, in case the json file contains multiple experiments] (default: {0})
+        
+        Returns:
+            [list] -- [The full slice of the nearest recorded timestamp]
+        """
+        timestamps = [float(i) for i  in self.data[experiment].keys()]
+
+        key = str(find_nearest(timestamps, time))
+
+        return self.data[experiment][key]
+
+    def get_index(self, index, experiment=0):
+        """[summary]
+        
+        Arguments:
+            index {[int]} -- [The index for which you want the data]
+        
+        Keyword Arguments:
+            experiment {int} -- [The experiment index, in case the json file contains multiple experiments] (default: {0})
+        
+        Returns:
+            [list] -- [The full slice at the specified index]
+        """
+        key = [self.data[experiment].keys()][n]
+        return self.data[experiment][key]
 
 if __name__ == '__main__':
     print("Afternoon file")
